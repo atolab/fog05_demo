98c48767e5c4469d1ef2899a6c86151752ad0b33
gabrik <gabriele.baldoni@gmail.com>
2018-12-18 16:10:44 +0100

    cpu ram usage script

---
+++ measurements/scripts/htop.py
@@ -0,0 +1,30 @@
+import psutil
+import scipy
+import time
+import signal
+import sys
+
+cpus = []
+ram = []
+
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    data = {
+        'cpu': cpus,
+        'mem': ram
+    }
+    scipy.io.savemat('htop.mat', data)
+    sys.exit(0)
+
+
+def main():
+    while True:
+        cpus.append(psutil.cpu_percent())
+        ram.append(psutil.virtual_memory()._asdict()/1024)
+        time.sleep(0.1)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    main()

22c3394cd4b0063f43762add1631eb4030bb6d56
gabrik <gabriele.baldoni@gmail.com>
2019-01-04 11:50:38 +0100

    indent fix
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- measurements/scripts/k8s/sc/bench.py
+++ measurements/scripts/k8s/sc/bench.py
@@ -21,21 +21,21 @@ def create_deployment_object(name, port):
         image="nginx:1.7.9",
         ports=[client.V1ContainerPort(container_port=port)])
     # Create and configurate a spec section
-        template = client.V1PodTemplateSpec(
-            metadata=client.V1ObjectMeta(labels={"app": "nginx"}),
-            spec=client.V1PodSpec(containers=[container]))
-        # Create the specification of deployment
-        spec = client.ExtensionsV1beta1DeploymentSpec(
-            replicas=1,
-            template=template)
-        # Instantiate the deployment object
-        deployment = client.ExtensionsV1beta1Deployment(
-            api_version="extensions/v1beta1",
-            kind="Deployment",
-            metadata=client.V1ObjectMeta(name=name),
-            spec=spec)
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={"app": "nginx"}),
+        spec=client.V1PodSpec(containers=[container]))
+    # Create the specification of deployment
+    spec = client.ExtensionsV1beta1DeploymentSpec(
+        replicas=1,
+        template=template)
+    # Instantiate the deployment object
+    deployment = client.ExtensionsV1beta1Deployment(
+        api_version="extensions/v1beta1",
+        kind="Deployment",
+        metadata=client.V1ObjectMeta(name=name),
+        spec=spec)
 
-        return deployment
+    return deployment
 
 
 def main(tries, chain_length):

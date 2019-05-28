651547a868faf55acf6c1b601f05844092a09427
gabrik <gabriele.baldoni@gmail.com>
2019-05-28 16:02:12 +0200

    updated webui container build
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- eclipse/view/build.sh
+++ eclipse/view/build.sh
@@ -5,13 +5,16 @@ lxc launch images:alpine/3.6 webui
 
 lxc exec webui -- apk update
 lxc exec webui -- apk add nodejs-npm nginx nodejs
-lxc exec webui -- cd /var/www/localhost/htdocs/ && npm install mqtt
+lxc exec webui -- npm install mqtt
 
 lxc file push ./templates/interfaces webui/etc/network/interfaces
 lxc file push ./templates/backend webui/etc/init.d/backend
-lxc file push ./templates/backend.js /webui/var/www/localhost/htdocs/backend.js
-lxc file push -r ./templates/view /webui/var/www/localhost/htdocs/
+lxc file push ./templates/backend.js webui/var/www/localhost/htdocs/backend.js
+lxc file push -r ./templates/webui webui/var/www/localhost/htdocs/
+lxc file push -r ./templates/default.conf webui/etc/nginx/conf.d/default.conf
 
+
+lxc exec webui -- chmod +x /etc/init.d/backend
 lxc exec webui -- rc-update add backend
 lxc exec webui -- rc-update add nginx
 

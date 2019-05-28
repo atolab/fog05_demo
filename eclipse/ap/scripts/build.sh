c6d75579e505f091ac4f8a6ee75ffb2a0da5ae47
gabrik <gabriele.baldoni@gmail.com>
2019-05-28 12:34:44 +0200

    added demo for ap with build script
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

---
+++ eclipse/ap/scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WFACE=$1
+
+lxc profile copy default ap_p
+lxc profile device add ap_p $WFACE nic nitctype=physical parent=$WFACE
+
+echo $WFACE | xargs -i sed -i -e "s/wlan/{}/g" ../templates/interfaces
+
+lxc launch images:alpine/3.9 ap -p ap_p
+
+lxc exec ap -- apk update
+lxc exec ap -- apk upgrade
+
+lxc exec ap -- apk add hostapd dnsmasq bridge
+lxc exec ap -- rc-update add hostapd
+lxc exec ap -- rc-update add dnsmasq
+
+lxc file push ../templates/interfaces ap/etc/network/interfaces
+lxc file push ../templates/hostapd.conf ap/etc/hostapd/hostapd.conf
+
+
+

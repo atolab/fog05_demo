af1270dd14ebc387168398888519fa273cbcc218
gabrik <gabriele.baldoni@gmail.com>
2019-05-28 16:26:37 +0200

    fix in view build script
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- eclipse/view/build.sh
+++ eclipse/view/build.sh
@@ -2,7 +2,7 @@
 
 
 lxc launch images:alpine/3.6 webui
-
+sleep 1;
 lxc exec webui -- apk update
 lxc exec webui -- apk add nodejs-npm nginx nodejs
 lxc exec webui -- npm install mqtt
@@ -12,7 +12,7 @@ lxc file push ./templates/interfaces webui/etc/network/interfaces
 lxc file push ./templates/backend webui/etc/init.d/backend
 lxc file push ./templates/backend.js webui/var/www/localhost/htdocs/backend.js
 lxc file push -r ./templates/webui webui/var/www/localhost/htdocs/
-lxc file push -r ./templates/default.conf webui/etc/nginx/conf.d/default.conf
+lxc file push ./templates/default.conf webui/etc/nginx/conf.d/default.conf
 
 
 lxc exec webui -- chmod +x /etc/init.d/backend
